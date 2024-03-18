a506a0f9f984a89b999d7bbd1d096328303064c7
VitaliyDolceVita <47143915+VitaliyDolceVita@users.noreply.github.com>
2024-03-18 23:35:11 +0200

    Update postgres.sql

--- postgres.sql
+++ postgres.sql
@@ -21,3 +21,22 @@ CREATE TABLE tasks (
     FOREIGN KEY (status_id) REFERENCES status(id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
+
+SELECT * FROM tasks WHERE user_id = 1;
+Оновити статус конкретного завдання
+sql
+Copy code
+UPDATE tasks SET status_id = (SELECT id FROM status WHERE name = 'in progress') WHERE id = 5;
+Додати нове завдання для конкретного користувача
+sql
+Copy code
+INSERT INTO tasks (title, description, status_id, user_id) VALUES ('Нове завдання', 'Опис нового завдання', 1, 2);
+Отримати список користувачів, які не мають жодного завдання
+sql
+Copy code
+SELECT * FROM users WHERE id NOT IN (SELECT user_id FROM tasks);
+Отримати кількість завдань для кожного статусу
+sql
+Copy code
+SELECT status.name, COUNT(tasks.id) FROM status LEFT JOIN tasks ON status.id = tasks.
